dd341c938b572bec38d6b076ed9340679df1af62
Jothika2411 <jothikajoe2411@gmail.com>
2024-05-30 01:45:24 +0530

    commit

---
+++ functions.js
@@ -0,0 +1,17 @@
+//Simple JS code in functions
+let x=10;
+a();
+b();
+console.log(x)
+
+
+function a(){
+    var x=1;
+    console.log(x)
+}
+
+function b(){
+    var x=100;
+    console.log(x)
+}
+
